86295cafecf0ccc3753d213b06772f76282f6c16
Nina Recchia <itsnina@pm.me>
2026-01-04 03:56:20 -0500

    Implements basic key/lock functionality

--- Source/DungeonEscape/CollectableItem.cpp
+++ Source/DungeonEscape/CollectableItem.cpp
@@ -9,6 +9,7 @@ ACollectableItem::ACollectableItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Tags.Add(FName("CollectableItem"));
 }
 
 // Called when the game starts or when spawned
---
+++ Source/DungeonEscape/Lock.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Lock.h"
+
+// Sets default values
+ALock::ALock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+	SetRootComponent(RootComp);
+
+	TriggerComp = CreateDefaultSubobject<UTriggerComponent>(TEXT("Trigger Component"));
+	TriggerComp->SetupAttachment(RootComp);
+
+	KeyItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Key Item Mesh"));
+	KeyItemMesh->SetupAttachment(RootComp);
+
+	Tags.Add(FName("Lock"));
+}
+
+// Called when the game starts or when spawned
+void ALock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetIsKeyPlaced(true);
+}
+
+// Called every frame
+void ALock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+bool ALock::GetIsKeyPlaced()
+{
+	return IsKeyPlaced;
+}
+
+void ALock::SetIsKeyPlaced(bool NewIsKeyPlaced)
+{
+	IsKeyPlaced = NewIsKeyPlaced;
+
+	TriggerComp->Trigger(NewIsKeyPlaced);
+	KeyItemMesh->SetVisibility(NewIsKeyPlaced);
+}
+
